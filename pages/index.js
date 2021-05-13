952830967223772eb75233273d9e57353b6e0649
hoang534 <whalermei@gmail.com>
2021-05-12 21:53:55 -0700

    updated project

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import {useState, useCallback, useRef} from 'react'
 import styles from '../styles/Home.module.css'
 import Search from './search.js'
 
 export default function Home() {
-  return (
   
-      
+  return (
 
-      <Search />
-    
-  )
-}
+    < Search / >
+
+    )
+  }
